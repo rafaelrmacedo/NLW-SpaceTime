082be38a73576f481a40b2752a90cdad701fd9bf
rafaelrodriguesmacedo2004@gmail.com <rafaelrodriguesmacedo2004@gmail.com>
2023-05-19 19:26:25 -0300

    some updates

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,11 @@ import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
 import { memoriesRoutes } from './routes/memories';
+import { authRoutes } from './routes/auth';
 
 const server = fastify()
 
+server.register(authRoutes)
 server.register(memoriesRoutes) 
 
 server.register(cors, {
@@ -17,7 +19,7 @@ server.register(jwt, {
 })
 
 server.listen({ //Como a promisse no JavaScript sigfica algo que possa demorar, então é necessário usar o then para que o servidor espere a promisse ser resolvida
-    port: 3000,
+    port: 3333,
 }).then(() => { //Anonymus function = função sem nome
     console.log('Server is running on port 3000');  
 })
